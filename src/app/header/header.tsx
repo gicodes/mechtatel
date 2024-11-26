0f7a43a8ba0f49cd4bea6f85a15d71af86148685
Gideon I <gicodes9@gmail.com>
2024-11-26 12:44:34 +0100

    header area

--- src/app/header/header.tsx
+++ src/app/header/header.tsx
@@ -47,9 +47,9 @@ const Header = () => {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
               </svg>
             </button>
-            <div className='pl-5'>
+            {/* <div className='pl-5'>
               <a className="logo-title" href='/'>M</a>
-            </div>
+            </div> */}
           </div>
 
           <div className='hidden lg:flex lg:gap-x-12 px-6'>
